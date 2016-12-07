e44a30f4177285b902a0f53e4484d1e985594aee
pedagova <permuta.yt@gmail.com>
2016-12-07 10:39:26 +0100

    2º sesión
    hasta el ejercicio 10

--- Practicas/Practica1/Practica1.sql
+++ Practicas/Practica1/Practica1.sql
@@ -29,3 +29,12 @@ WHERE department_name = 'Sales');
 SELECT distinct first_name, employees.employee_id
    FROM employees, A1
    WHERE employee_id  = A1.ident;
+   
+select distinct e2.first_name
+from employees e1, employees e2
+where (e1.employee_id is not null) and (e2.employee_id = e1.manager_id);
+
+
+select distinct e2.first_name, e1.first_name, e2.salary, e1.salary
+from employees e1, employees e2
+where (e1.employee_id is not null) and (e2.employee_id = e1.manager_id) and e2.salary < e1.salary;
